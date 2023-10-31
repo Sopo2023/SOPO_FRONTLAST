fb56501573e5ef8e0d2bb87eca9f9594d8de9e6e
je355 <120763503+je355@users.noreply.github.com>
2023-10-31 19:49:10 +0900

    .

--- SOPOLAST/src/Components/Sidebar/side.js
+++ SOPOLAST/src/Components/Sidebar/side.js
@@ -10,58 +10,57 @@ export default function Side(){
     return(
         <div className="side">
         <div className='proBox_2'>
-          <div className='proBox_1'></div>
-        </div>
-        <div className="boxContents">
-          <div className='proName'>
-            <div className="nameSide">
-              이해준
-            </div>
-            {/* <div className='proHr'>
+          <div className='proBox_1'>
+
+
+            <div className="allContainer">
               
-            </div> */}
-
-            <hr className='nameHr'/>
-            <div className='proBio'>
-              <span className='firstBio'>안녕하세요.<br />프론트엔드 개발자 이해준입니다.</span>
-              <div className='secondBio'> 매일 성장하는 개발자 이해준입니다.</div>
-              <div className='skillTitle'>skills</div>
-              <div>React / HTML / CSS / JS / C</div>
-              <div className='awardTitle'>Award</div>
-              <div>🥇 교내해커톤 대상<br></br>🏆 스마틴 앱 챌린지 우수상 <br></br>🎖️ ICT 대구 교육감상</div>
-
-                <div className='bioInfo'>
-                  <div className='oddBio'>
-                    <img src={hp} className='imgBio'></img>
-                    <span className='perBio'> 010-1234-1234</span>
+              <div className="boxContents">
+                <div className='proName'>
+                  <div className="nameSide">
+                    이해준
                   </div>
 
-                  <div className='oddBio'>
-                    <img src={org} className='imgBio'></img>
-                    <span className='perBio'> B1ND</span>
-                  </div>
+                  <hr className='nameHr'/>
+                  <div className='proBio'>
+                    <span className='firstBio'>안녕하세요.<br />프론트엔드 개발자 이해준입니다.</span>
+                    <div className='secondBio'> 매일 성장하는 개발자 이해준입니다.</div>
+                    <div className='skillTitle'>skills</div>
+                    <div>React / HTML / CSS / JS / C</div>
+                    <div className='awardTitle'>Award</div>
+                    <div>🥇 교내해커톤 대상<br></br>🏆 스마틴 앱 챌린지 우수상 <br></br>🎖️ ICT 대구 교육감상</div>
 
-                  <div className='oddBio'>
-                    <img src={mail} className='imgBio'></img>
-                    <span className='perBio'> leehaejoon@dgsw.hs.kr</span>
-                  </div>
+                      <div className='bioInfo'>
 
-                  <div className='oddBio'>
-                    <img src={github} className='imgBio'></img>
-                    <span className='perBio'> githae </span>
-                  </div>
+                        <div className='oddBio'>
+                          <img src={org} className='imgBio'></img>
+                          <span className='perBio'> B1ND</span>
+                        </div>
 
-                  <div className='oddBio'>
-                    <img src={insta} className='imgBio'></img>
-                    <span className='perBio'> @haeejoon1234</span>
-                  </div>
+                        <div className='oddBio'>
+                          <img src={mail} className='imgBio'></img>
+                          <span className='perBio'> leehaejoon@dgsw.hs.kr</span>
+                        </div>
 
-                  <div className='oddBio'>
-                    <img src={kakao} className='imgBio'></img>
-                    <span className='perBio'> imhaejoon </span>
-                  </div>
+                        <div className='oddBio'>
+                          <img src={github} className='imgBio'></img>
+                          <span className='perBio'> githae </span>
+                        </div>
 
-                </div> 
+                        <div className='oddBio'>
+                          <img src={insta} className='imgBio'></img>
+                          <span className='perBio'> @haeejoon1234</span>
+                        </div>
+
+                        <div className='oddBio'>
+                          <img src={kakao} className='imgBio'></img>
+                          <span className='perBio'> imhaejoon </span>
+                        </div>
+
+                      </div> 
+            </div>
+          </div>
+        </div>
                 {/* bioInfo */}
               </div> 
               {/* award */}
