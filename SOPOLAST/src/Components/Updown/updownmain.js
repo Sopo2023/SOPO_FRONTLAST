17f842cabcbff69c33545a8ca22346436e039648
박규민 <131234408+kyumin7487@users.noreply.github.com>
2023-11-02 23:02:23 +0900

    fix :: 게시물공간 문제 수정
    게시물 공간이 다 따로 되있어서 고쳤습니다

--- SOPOLAST/src/Components/Updown/updownmain.js
+++ SOPOLAST/src/Components/Updown/updownmain.js
@@ -21,7 +21,6 @@ export default function Start() {
           <input type="button" className="search_icon"></input>
           <div className="searcg_icon_line"></div>
         </div>
-
         <div className="post-container">
           <div className="post-written">
             <div className="post_border">
