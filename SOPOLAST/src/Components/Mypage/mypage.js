ab9f4753074fe30739bb5d4e0873a6057e3dc623
박규민 <131234408+kyumin7487@users.noreply.github.com>
2023-11-03 23:47:08 +0900

    ♻ : About Me 코드 리펙토링

--- SOPOLAST/src/Components/Mypage/mypage.js
+++ SOPOLAST/src/Components/Mypage/mypage.js
@@ -13,7 +13,9 @@ export default function Mypage() {
         <p className="top2">DGSW 8th</p>
         <br></br>
         <div className="fixbutton" onClick={()=>{navigate("/Mypagesub")}}>
-          프로필 수정하기
+          <div className="fixbuttonT">
+              프로필 수정하기
+          </div>
         </div>
       </div>
       <div className="Readme">
@@ -25,36 +27,38 @@ export default function Mypage() {
       <div className="social">
         <p className="head2">About Me</p>
         <div className="socialp">
-        <p>
-          <Link to="https://github.com/ftery0" className="link1">
-            깃:
-          </Link>
-          <Link to="https://github.com/ftery0" className="link2">@ftery0</Link>
-        </p>
-        <p>
-          <Link to="/mypage/mail" className="link1">
-            메일:
-          </Link>
-          <a href="mailto:lerb5253@dgsw.hs.kr"> 이해준님께 메일쓰기 </a>
-        </p>
-        <p>
-          <Link to="/mypage/number" className="link1">
-            전화번호:
-          </Link>
-          010-1234-5678
-        </p>
-        <p>
-          <Link to="https://www.instagram.com/hae_jun7388/" className="link1">
-            인스타:
-          </Link>
-          <Link to="https://www.instagram.com/hae_jun7388/" className="link2">@hae_jun7388</Link>
-        </p>
-        <p>
-          <Link to="/mypage/kako" className="link1">
-            카톡:
-          </Link>
-          이해준
-        </p>
+          <div className="pT">
+              <p>
+              <Link to="https://github.com/ftery0" className="link1">
+                깃:
+              </Link>
+              <Link to="https://github.com/ftery0" className="link2">@ftery0</Link>
+            </p>
+            <p>
+              <Link to="/mypage/mail" className="link1">
+                메일:
+              </Link>
+              <a href="mailto:lerb5253@dgsw.hs.kr"> 이해준님께 메일쓰기 </a>
+            </p>
+            <p>
+              <Link to="/mypage/number" className="link1">
+                전화번호:
+              </Link>
+              010-1234-5678
+            </p>
+            <p>
+              <Link to="https://www.instagram.com/hae_jun7388/" className="link1">
+                인스타:
+              </Link>
+              <Link to="https://www.instagram.com/hae_jun7388/" className="link2">@hae_jun7388</Link>
+            </p>
+            <p>
+              <Link to="/mypage/kako" className="link1">
+                카톡:
+              </Link>
+              이해준
+            </p>
+          </div>
         </div>
       </div>
       {/* <div className="post_mainnn"> */}
@@ -63,9 +67,11 @@ export default function Mypage() {
         <div className="post-write">
           <div className="Zonecontrol">
             <span className="Name">이해준</span>
-            <span className="title">오늘 먹은거 </span>
+            <span className="title">4개교 연합 해커톤 프론트엔드  </span>
             <span className="detail">
-              햄버거 치킨 피자 짜장면 마라탕 탕후루 사과 탕수육 치즈 
+            2023년 08월 24일 4개교 연합 해커톤을 진행했다.고종현 님 @manudeli 경력 요기요에서
+            -Merchant trine : 사장님 관련 서비스 사장 사이트, 사장 관리자 서비스 공통 요기요 디자인, FE
+            나의 이야기 제대 전, 수포자 고등학생에서 미대생(디자인전공)으로서의 삶...
             </span>
             <span className="date">2023.09.22</span>
           </div>
