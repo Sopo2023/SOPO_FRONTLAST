32724ee7f8f288421b7348b689d075f7b68916c4
박규민 <131234408+kyumin7487@users.noreply.github.com>
2023-11-07 10:13:59 +0900

    feat :카카오톡 아이디 입력기능, 이메일 기능 추가

--- SOPOLAST/src/Components/Mypage/mypage.js
+++ SOPOLAST/src/Components/Mypage/mypage.js
@@ -311,6 +311,18 @@ export default function Mypage() {
                     type="text"
                     placeholder="당신의 수상기록을 입력해주세요.(최대3개)"
                     ></input>
+                    <span className="writing8"> 당신의 이메일은 무엇인가요? </span>
+                    <input 
+                    id="email" 
+                    type="text"
+                    placeholder="당신의 이메일을 입력해주세요."
+                    ></input>
+                    <span className="writing9"> 당신의 카카오톡은 무엇인가요? </span>
+                    <input 
+                    id="kk" 
+                    type="text"
+                    placeholder="당신의 카카오톡 아이디를 입력해주세요."
+                    ></input>
                     <div className="completebutton">
                     완료
                     </div>
