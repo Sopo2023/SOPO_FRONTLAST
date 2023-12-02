a3dbe05413c96ccb0fe2a6c92347f1a747d64eeb
해준 <127281057+ftery0@users.noreply.github.com>
2023-12-02 17:06:56 +0900

    ♻ :: toast 삭제

--- SOPOLAST/src/Components/Signup/signup.jsx
+++ SOPOLAST/src/Components/Signup/signup.jsx
@@ -29,18 +29,6 @@ function LoginComponent() {
   const handleEmailCertify = async (e) => {
     e.preventDefault();
     setIsCertifying(true);
-    const Toast = Swal.mixin({
-      toast: true,
-      position: "top-end",
-      showConfirmButton: false,
-      timer: 2000,
-      timerProgressBar: true,
-      didOpen: (toast) => {
-        toast.addEventListener("mouseenter", Swal.stopTimer);
-        toast.addEventListener("mouseleave", Swal.resumeTimer);
-      },
-    });
-
     if (!email) {
       showToast("warning", "이메일을 입력해 주세요");
       return;
