146642efad858e08a272a6dab77b7e685a417fb0
해준 <127281057+ftery0@users.noreply.github.com>
2024-01-17 03:31:21 +0900

    design :: 글쓰기 디자인 수정

--- SOPOLAST/src/Components/Sidewrite/write.tsx
+++ SOPOLAST/src/Components/Sidewrite/write.tsx
@@ -4,11 +4,13 @@ import axios from "axios";
 import Head from "../../constants/head/Head/head";
 import Side from "../../constants/Sidebar/Side/side";
 import "./write.css";
-import Swal from "sweetalert2";
+import ImgPlus from "src/Assets/image/imgplus.png";
+import SubmitImg from "src/Assets/image/submitimg.png";
+import * as s from "./Write.style";
+import { showToast } from "src/constants/Swal/Swal";
 
-interface SidewriteProps {}
 
-const Sidewrite: React.FC<SidewriteProps> = () => {
+const Sidewrite = () => {
   const [title, setTitle] = useState<string>("");
   const [content, setContent] = useState<string>("");
   const [imageSrc, setImageSrc] = useState<string | null>(null);
@@ -16,17 +18,6 @@ const Sidewrite: React.FC<SidewriteProps> = () => {
   const [fileName, setFileName] = useState<string | null>(null);
   const localStorageEmail = localStorage.getItem("sopo_id");
   const SERVERURL = `${process.env.REACT_APP_SERVER_URL}`;
-  const Toast = Swal.mixin({
-    toast: true,
-    position: "top-end",
-    showConfirmButton: false,
-    timer: 2000,
-    timerProgressBar: true,
-    didOpen: (toast) => {
-      toast.addEventListener("mouseenter", Swal.stopTimer);
-      toast.addEventListener("mouseleave", Swal.resumeTimer);
-    },
-  });
 
   const onSubmitHandler = async (e: FormEvent) => {
     e.preventDefault();
@@ -38,10 +29,7 @@ const Sidewrite: React.FC<SidewriteProps> = () => {
     ).value;
 
     if (!title || !content) {
-      Toast.fire({
-        icon: "error",
-        title: "제목, 내용을 모두 입력해주세요.",
-      });
+      showToast("error","제목, 내용을 모두 입력해주세요.");
       return;
     }
 
@@ -87,62 +75,81 @@ const Sidewrite: React.FC<SidewriteProps> = () => {
   };
 
   return (
-    <div className="main">
-      <div className="content">
+    <>
+      <s.Main>
         <Head active={false} />
         <Side />
-        <form className="write_form" onSubmit={onSubmitHandler}>
-          <div className="write_img1">
-            {imageSrc && <img src={imageSrc} alt="Preview" />}
-          </div>
-          <div className="sc-cBornZ-gegSAw">
-            <span>카테고리</span>
-            <select className="sc-cBornz-gegs">
-              <option value="웹">웹</option>
-              <option value="서버">서버</option>
-              <option value="안드">안드로이드</option>
-              <option value="iOS">iOS</option>
-              <option value="임베">임베디드</option>
-              <option value="디자인">디자인</option>
-              <option value="기타">기타</option>
-            </select>
-          </div>
-          <div className="sd-cBornZ-gegSAw">
-            <span>올릴곳</span>
-            <select className="sd-cBornz-gegs">
-              <option value="게시물">게시물</option>
-              <option value="대회">대회</option>
-            </select>
-          </div>
-          <input
-            className="write_title"
-            type="text"
-            placeholder="제목"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <textarea
-            className="write_screen"
-            placeholder="내용"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          ></textarea>
-          <label htmlFor="file">
-            <div className="btn-upload">사진 설정</div>
-          </label>
-          <input
-            type="file"
-            name="file"
-            id="file"
-            onChange={handleImageChange}
-          />
-          {fileName && <div>{fileName}</div>}
-          <button id="write_submit" type="submit">
-            Submit
-          </button>
-        </form>
-      </div>
-    </div>
+        <s.Write_Main>
+          <s.Writing>
+            <s.WriteTool>
+              <s.Tool>
+                <span>올릴곳</span>
+                <s.CategorySelect>
+                  <option value="게시물">게시물</option>
+                  <option value="대회">대회</option>
+                </s.CategorySelect>
+              </s.Tool>
+              <s.Tool>
+                <span>카테고리</span>
+                <s.CategorySelect>
+                  <option value="웹">웹</option>
+                  <option value="서버">서버</option>
+                  <option value="안드">안드로이드</option>
+                  <option value="iOS">iOS</option>
+                  <option value="임베">임베디드</option>
+                  <option value="디자인">디자인</option>
+                  <option value="기타">기타</option>
+                </s.CategorySelect>
+              </s.Tool>
+              <s.Tool>
+                <p>이미지추가</p>
+                <s.plustimg>
+                  <img src={ImgPlus} />
+                </s.plustimg>
+              </s.Tool>
+              <s.Tool>
+              <s.SubmitButton type="submit">Submit</s.SubmitButton>
+              <s.plustimg>
+              <img src={SubmitImg}/>
+              </s.plustimg>
+              </s.Tool>
+            </s.WriteTool>
+            <s.WriteUnder>
+            <s.WriteForm onSubmit={onSubmitHandler}>
+            {/* <div className="write_img1">
+              {imageSrc && <img src={imageSrc} alt="Preview" />}
+            </div> */}
+      <s.WriteTitlelMain>
+            <s.WriteTitle
+              type="text"
+              placeholder="제목"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            </s.WriteTitlelMain>
+            <s.WriteDetailMain>
+            <s.WriteDetail
+              placeholder="내용"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            ></s.WriteDetail>
+            </s.WriteDetailMain>
+            {/* <label htmlFor="file">
+              <div className="btn-upload">사진 설정</div>
+            </label>
+            <input
+              type="file"
+              name="file"
+              id="file"
+              onChange={handleImageChange}
+            /> */}
+            {/* {fileName && <div>{fileName}</div>} */}
+            </s.WriteForm>
+            </s.WriteUnder>
+          </s.Writing>
+        </s.Write_Main>
+      </s.Main>
+    </>
   );
 };
 
