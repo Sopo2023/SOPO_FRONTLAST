3b555e9efbe28f50399539d7ff0a8b7d5bd827fa
해준 <127281057+ftery0@users.noreply.github.com>
2024-01-16 10:26:21 +0900

    refactor :: 코드 수정

--- SOPOLAST/src/constants/Sidebar/Side/side.tsx
+++ SOPOLAST/src/constants/Sidebar/Side/side.tsx
@@ -35,49 +35,47 @@ export default function Side() {
             <s.ProName>{sideName}박규민 </s.ProName>
           </s.NameBox>
           <s.profile>
-          <s.proBorder>
-            <s.proMain>
-              <s.Span>
-                안녕하세요.
-                <br />
-                프론트엔드 개발자 {sideName}입니다.
-              </s.Span>
-              <s.Span>
-                {" "}
-                매일 성장하는 개발자 {sideName}입니다.
-              </s.Span>
-              <s.Span>skills</s.Span>
-              <s.SapnDetail>React / HTML / CSS / JS / C</s.SapnDetail>
-              <s.Span>Award</s.Span>
-              <s.SapnDetail>
-                🥇 교내해커톤 대상<br></br>🏆 스마틴 앱 챌린지 우수상 <br></br>
-                🎖️ ICT 대구 교육감상
-              </s.SapnDetail>
+            <s.proBorder>
+              <s.proMain>
+                <s.Span>
+                  안녕하세요.
+                  <br />
+                  프론트엔드 개발자 {sideName}입니다.
+                </s.Span>
+                <s.Span> 매일 성장하는 개발자 {sideName}입니다.</s.Span>
+                <s.Span>skills</s.Span>
+                <s.SapnDetail>React / HTML / CSS / JS / C</s.SapnDetail>
+                <s.Span>Award</s.Span>
+                <s.SapnDetail>
+                  🥇 교내해커톤 대상<br></br>🏆 스마틴 앱 챌린지 우수상{" "}
+                  <br></br>
+                  🎖️ ICT 대구 교육감상
+                </s.SapnDetail>
 
-              <s.ProImage>
-                <s.oodBio>
-                  <img src={org} className="imgBio"></img>
-                  <s.perBio> CNS</s.perBio>
-                </s.oodBio>
+                <s.ProImage>
+                  <s.oodBio>
+                    <s.imgBio src={org}></s.imgBio>
+                    <s.perBio> CNS</s.perBio>
+                  </s.oodBio>
 
-                <s.oodBio>
-                  <s.imgBio src={mail}></s.imgBio>
-                  <s.perBio> {email}</s.perBio>
-                </s.oodBio>
+                  <s.oodBio>
+                    <s.imgBio src={mail}></s.imgBio>
+                    <s.perBio> {email}</s.perBio>
+                  </s.oodBio>
 
-                <s.oodBio>
-                  <s.imgBio src={github1}></s.imgBio>
-                  <s.perBio> {github} </s.perBio>
-                </s.oodBio>
+                  <s.oodBio>
+                    <s.imgBio src={github1}></s.imgBio>
+                    <s.perBio> {github} </s.perBio>
+                  </s.oodBio>
 
-                <s.oodBio>
-                  <s.imgBio src={kakao}></s.imgBio>
-                  <s.perBio> 010-2612-2864 </s.perBio>
-                </s.oodBio>
-              </s.ProImage>
+                  <s.oodBio>
+                    <s.imgBio src={kakao}></s.imgBio>
+                    <s.perBio> 010-2612-2864 </s.perBio>
+                  </s.oodBio>
+                </s.ProImage>
               </s.proMain>
-            </s.proBorder>          
-            </s.profile>
+            </s.proBorder>
+          </s.profile>
         </s.allContainer>
       </s.proBox1>
     </s.side>
