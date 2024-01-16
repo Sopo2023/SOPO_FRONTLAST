099ae07e027cfe9436ac4d32f0f0966f825ccc82
je355 <chaeeehui@gmail.com>
2024-01-16 16:07:29 +0900

    `♻ ::` styled portfolio

--- SOPOLAST/src/Components/Portfolio/portfolioMain.style.ts
+++ SOPOLAST/src/Components/Portfolio/portfolioMain.style.ts
@@ -99,3 +99,148 @@ export const MajorSelect = styled.div`
     position: relative;
     
 `
+
+export const MajorButton = styled.button`
+    border: none;
+    background-color: none;
+    padding: 13px;
+    display: inline;
+    color: #A7A7A7;
+    font-family: Inter;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    position: relative;
+    background: none;
+    cursor: pointer;
+`
+
+export const MajorButtonHover = styled(MajorButton)`
+    &:hover {
+        color: #1A9A18 !important;
+    }
+`
+
+export const MajorButtonClicked = styled(MajorButton)`
+    &:active {
+        color: #1A9A18 !important;
+    }
+`
+
+export const Mo = styled.div`
+    position: absolute;
+    left: 460px;
+    top: 200px;
+    width: 900px;
+    height: 1000px;
+`
+
+export const NameCardBox = styled.div`
+    width: 840px;
+    height: 130px;
+    flex-shrink: 0;
+    border-radius: 10px;
+    background: #FFF;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    position: absolute;
+    margin-left: 22px;
+    margin-top: 30px;
+    display: inline-block;
+`;
+
+export const HoveredNameCardBox = styled(NameCardBox)`
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const CardName = styled.div`
+    color: #000;
+    font-family: Inter;
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 800;
+    line-height: normal;
+    position: absolute;
+    margin-top: 22px;
+    margin-left: 33px;
+`
+
+export const CardGrade = styled.div`
+    color: #8C8C8C;
+    font-family: Inter;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    position: absolute;
+    margin-top: 55px;
+    margin-left: 33px;
+`
+
+export const CardMail = styled.div`
+    color: #8C8C8C;
+    font-family: Inter;
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    position: absolute;
+    margin-top: 99px;
+    margin-left: 33px;
+`
+
+export const NameCardBox2 = styled.div`
+    width: 840px;
+    height: 130px;
+    flex-shrink: 0;
+    border-radius: 10px;
+    background: #FFF;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    position: absolute;
+    margin-left: 22px;
+    margin-top: 180px;
+    cursor: pointer;
+    display: inline-block;
+`;
+
+export const NameCardBox3 = styled.div`
+    width: 840px;
+    height: 130px;
+    flex-shrink: 0;
+    border-radius: 10px;
+    background: #FFF;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    position: absolute;
+    margin-left: 22px;
+    margin-top: 330px;
+    display: inline-block;
+`;
+
+export const NameCardBox4 = styled.div`
+    width: 840px;
+    height: 130px;
+    flex-shrink: 0;
+    border-radius: 10px;
+    background: #FFF;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    position: absolute;
+    margin-left: 22px;
+    margin-top: 480px;
+    display: inline-block;
+`;
+
+export const NameCardBox5 = styled.div`
+    width: 840px;
+    height: 130px;
+    flex-shrink: 0;
+    border-radius: 10px;
+    background: #FFF;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    position: absolute;
+    margin-left: 22px;
+    margin-top: 640px;
+    display: inline-block;
+`
+
