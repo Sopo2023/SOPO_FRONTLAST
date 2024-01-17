65ab3393f05ece4827a4049b104c184dd773f6eb
해준 <127281057+ftery0@users.noreply.github.com>
2024-01-17 17:46:40 +0900

    refactor : : 검색 코드 수정

--- SOPOLAST/src/constants/magnifying/Magnifying.Style.tsx
+++ SOPOLAST/src/constants/magnifying/Magnifying.Style.tsx
@@ -1,54 +1,53 @@
 import styled from "styled-components";
 
 export const Search1 = styled.div`
-    position: absolute;
-    top: 8%;
-    left: 580px;
-    width: 840px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width: 80%;
     height: 50px;
     flex-shrink: 0;
     border-radius: 50px;
     border: 1px solid #1A9A18;
     background: #FFF;
-    z-index: 2;
-    margin-top: 30px;
 `;
 export const Search_box = styled.input`
-    position: absolute;
+    margin-left: 20px;
+    display: flex;
     outline: none;
     font-size: 20px;
-    left: 20px;
-    top: 5px;
     border: none;
-    width: 750px;
-    height: 40px;
+    width: 90%;
+    height: 90%;
     display: flex;
 `;
-export const Search_icon = styled.input`
-    type="button"
-    position: absolute;
+export const Search_icon = styled.button`
     cursor: pointer;
+    outline: none;
     background-color: #FFF;
-    top: 9px;
-    left: 790px;
     width: 25px;
     height: 25px;
-    flex-shrink: 0;
     border: 1px solid #1A9A18;
     border-radius: 50%;
-    margin-left: 790px;
-    margin-top: 6px;
 `;
 export const Searcg_icon_line = styled.div`
-    position: absolute;
     cursor: pointer;
-    top: 35px;
-    left: 809px;
+    margin-top: 3px;
+    margin-left: 15px;
     background-color: #1A9A18;
-    transform: rotate( 50deg );
+    transform: rotate( 55deg );
     width: 13px;
     height: 1px;
     flex-shrink: 0;
     stroke-width: 1px;
     stroke: #1A9A18;
 `;
+export const SearcTool =styled.button`
+    display: flex;
+    margin-top: 15px;
+    flex-direction: column;
+    width: 35px;
+    height: 100%;
+    border: none;
+    background: none;
+`
--- SOPOLAST/src/constants/magnifying/Magnifying/Magnifying.tsx
+++ SOPOLAST/src/constants/magnifying/Magnifying/Magnifying.tsx
@@ -6,8 +6,10 @@ export default function Magnifying() {
     return (
         <s.Search1>
           <s.Search_box></s.Search_box>
+          <s.SearcTool>
           <s.Search_icon></s.Search_icon>
           <s.Searcg_icon_line></s.Searcg_icon_line>
+          </s.SearcTool>
         </s.Search1>
     )
 }
