690010d9f973f473bbb19b2254ed4273f9a1857a
je355 <chaeeehui@gmail.com>
2024-01-18 17:40:32 +0900

    `♻ ::` styled-compenents

---
+++ SOPOLAST/src/Components/Portfoliosub/portfolioSub.style.ts
@@ -0,0 +1,271 @@
+import styled from "styled-components";
+
+export const MoPortfolio = styled.div`
+    position: absolute;
+    left: 20%;
+    width: 70%;
+    height: 100%;
+    
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+export const AllPort = styled.div`
+    width: 80%;
+    height: 100%;
+    /* background-color: #f6f6f6; */
+    position: absolute;
+    display: flex; 
+    flex-direction: column; 
+    /* align-items: center;*/
+    justify-content: center; 
+`
+
+export const PortfoioProfile = styled.div`
+    width: 57%;
+    height: 200px;
+  
+`
+
+export const NamePort = styled.div`
+    color: #000;
+    font-family: Inter; 
+    font-size: 40px;
+    font-style: normal;
+    font-weight: 800;
+    line-height: normal;
+`
+
+export const GradePort = styled.div`
+    color: #000;
+
+    font-family: Inter;
+    font-size: 30px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+`
+
+export const BioPort = styled.p`
+    color: #8C8C8C;
+
+    font-family: Inter;
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+`
+
+export const SetBio = styled.p`
+    color: #000;
+    font-family: Inter;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 900;
+    line-height: normal;
+`
+
+export const PortfolioSkill = styled.div`
+    width: 57%;
+    height: 200px;
+`
+
+export const SkillsBio = styled.p`
+    margin-top: 9px;
+
+    color: #000;
+
+    font-family: Inter;
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 900;
+    line-height: normal;
+`
+
+export const AboutSkills = styled.p`
+    margin-top: 2px;
+
+    color: #000;
+
+    font-family: Inter;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+`
+
+export const PortfoioAward = styled.div`
+    width: 57%;
+    height: 100px;
+`
+
+export const AwardBio = styled.p`
+    color: #000;
+
+    font-family: Inter;
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 900;
+    line-height: normal;
+`
+
+export const AboutAward = styled.p`
+    color: #000;
+
+    font-family: Inter;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+`
+
+export const PortfoiloAboutMe = styled.div`
+  width: 57%;
+  height: 100px;
+  
+  margin-top: 20px;
+`
+
+export const AboutMe = styled.p`
+    margin-top: 21px;
+
+    color: #000;
+
+    font-family: Inter;
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 900;
+    line-height: normal;
+`
+
+export const MorePort = styled.p`
+    margin-top: 7px;
+
+    color: #000;
+
+    font-family: Inter;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 900;
+    line-height: normal;
+`
+
+export const HerfPort = styled.a`
+  margin-top: 2px;
+  
+  color: #1A9A18;
+
+  font-family: Inter;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`
+
+export const PortMore = styled.div`
+  margin-top: 2rem;
+  width: 57%;
+  height: 200px;
+  margin-top: 20px;
+`
+
+export const AboutPort = styled.img`
+  width: 20px;
+  height: 20px;
+`
+
+export const AboutText = styled.p`
+    display: inline-block;
+
+    color: #000;
+
+    font-family: Inter;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+
+    text-decoration: none;
+
+    margin-left: 1rem;
+`
+export const Aboutport = styled.img`
+  width: 20px;
+  height: 20px;
+`
+
+export const Abouttext = styled.a`
+    display: inline-block;
+
+    color: #000;
+
+    font-family: Inter;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+
+    text-decoration: none;
+
+    margin-left: 1rem;
+`
+
+export const MoreText = styled.div`
+  margin-top: 3rem;
+  width: 57%;
+  height: 200px;
+`
+
+export const MyStroy = styled.div`
+    color: #1A9A18;
+
+    font-family: Inter;
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 900;
+    line-height: normal;
+`
+
+export const PlusButton = styled.div`
+    display: inline;
+    position: absolute;
+`
+
+export const HrGreen = styled.div`
+  width: 100%;
+  border: #1A9A18 solid;
+  margin-top: 10px;
+`
+
+export const AboutTitle = styled.div`
+    font-family: Inter;
+    font-size: 17px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+
+    margin-top: 38px;
+`
+
+export const EachTitle = styled.p`
+  margin-top: 5px;
+  margin-bottom: 5px;
+`
+
+export const MoreContent = styled.div`
+  width: 57%;
+  height: 200px;
+`
+
+export const MyCompi = styled.div`
+  margin-top: 50px;
+  color: #1A9A18;
+
+    font-family: Inter;
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 900;
+    line-height: normal;
+`
